db9935a4e546bdfc366c3c8d552ff18b2e2e7297
Logandenver <99253111+Logandenver@users.noreply.github.com>
2022-02-08 19:45:49 +0530

    Add files via upload

---
+++ #assignment 2.py
@@ -0,0 +1,7 @@
+#assignment 2
+#program to convert kilometres to centimetres
+km=float(input("enter value in kilometres: "))
+#km is kilometres,cm is centimetres and m is metres
+#1km = 1000m and 1m = 100cm so we combined two formulas to one
+cm=(km*1000)*100
+print(km,"kilometres is:",cm,"centimetres")
