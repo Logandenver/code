6d465c1089d0b907f8ad659023bf3c6e68206e1f
Logandenver <99253111+Logandenver@users.noreply.github.com>
2022-02-09 19:01:38 +0530

    Add files via upload

---
+++ 09-02-2022-assign2.py
@@ -0,0 +1,8 @@
+#assignment 2
+#program to check if given number is odd or even
+#recieving input from user
+a=int(input("enter a value: "))
+if a%2=0:
+    print(a,"is even number")
+else:
+    print(a,"is odd number")
