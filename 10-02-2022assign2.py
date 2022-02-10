a81f4423d9c9577eb28c46244d1c7ef455189263
Logandenver <99253111+Logandenver@users.noreply.github.com>
2022-02-10 18:26:11 +0530

    Add files via upload

---
+++ 10-02-2022assign2.py
@@ -0,0 +1,8 @@
+#assignment 2
+#program to check whether a given number is positive number or not.
+print("-----To check postive or negative number-----")
+n=int(input("enter a number: "))
+if n<0:
+    print(n,"is a negative number")
+else:
+    print(n,"is a positive number")
