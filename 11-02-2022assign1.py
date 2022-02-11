b8d3d8d56a4390e6bcf45d79c7b7696154a81122
Logandenver <99253111+Logandenver@users.noreply.github.com>
2022-02-11 20:43:17 +0530

    Add files via upload

---
+++ 11-02-2022assign1.py
@@ -0,0 +1,14 @@
+#assignment 1
+#if else problem from Hacker Rank
+n = int(input())
+if n % 2 == 0:
+    if n in range(2,6):
+        print("Not Weird")
+
+    elif n in range(6,21):
+        print("Weird")
+
+    elif n > 20:
+        print("Not Weird")
+else:
+    print("Weird")
