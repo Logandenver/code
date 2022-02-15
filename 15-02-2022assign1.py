595b4b7b8bcf123caa6ebab13079729f993bacbf
Logandenver <99253111+Logandenver@users.noreply.github.com>
2022-02-15 18:28:25 +0530

    Add files via upload

---
+++ 15-02-2022assign1.py
@@ -0,0 +1,9 @@
+#assignment 1
+#program to perform arithmetic operations 
+print("-------Enter values-------"))
+a=int(input("a: "))
+b=int(input("b: "))
+
+print("addition operation: ",a+b)
+print("subtraction operation: ",a-b)
+print("multiplication operation: ",a*b)
---
+++ 15-02-2022assign2.py
@@ -0,0 +1,8 @@
+#Assignment 2
+#program to perform arithametic operations
+print("------Enter values-----")
+a=int(input("a: "))
+b=int(input("b: "))
+
+print("Integer division: ",int(a/b))
+print("Float division: ",float(a/b))
