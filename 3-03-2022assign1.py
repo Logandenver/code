c1c1df2f3d15b5b3b76ac47505bb93d8fbbf4a7c
Logandenver <99253111+Logandenver@users.noreply.github.com>
2022-03-03 20:01:52 +0530

    Add files via upload

---
+++ 3-03-2022assign1.py
@@ -0,0 +1,4 @@
+#assignment 1
+n=int(input("enter range: "))
+a=[i for i in range(n,0,-1)]
+print(a)
---
+++ 3-03-2022assign2.py
@@ -0,0 +1,5 @@
+#assignment 2
+a=('logan','denver','richard','evangelin','katherine')
+b=(1,2,3,4,5)
+print(a+b)
+        
